671702296d6e0959a1af95860a7709a985741356
bs-jenk <jenkinson.sam@gmail.com>
2024-11-20 15:28:20 +0000

    extract main header into separate component

--- inkwell/app/page.tsx
+++ inkwell/app/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <header className="text-center">
-      <h1 className=" text-9xl m-4">InkWell</h1>
-      <button className="border rounded-full p-7 my-10 hover:bg-green-600 hover:text-white transition duration-300 ease-in">
-        <Link href="/log-in">Log in</Link>
-      </button>
-    </header>
+    <>
+      <Header />
+      <div className="text-center">
+        <button className="border rounded-full p-7 my-10 hover:bg-green-600 hover:text-white transition duration-300 ease-in">
+          <Link href="/log-in">Log in</Link>
+        </button>
+      </div>
+    </>
   );
 }
