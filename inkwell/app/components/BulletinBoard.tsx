83d575b1c9cd6b3d5a7f78648cfb5de275f2c8cd
bs-jenk <jenkinson.sam@gmail.com>
2024-11-21 16:28:17 +0000

    adjust styling

--- inkwell/app/components/BulletinBoard.tsx
+++ inkwell/app/components/BulletinBoard.tsx
@@ -1,13 +1,12 @@
-// import Image from "next/image";
+import Image from "next/image";
 import React from "react";
 
 const BulletinBoard = () => {
   return (
-    <div>
-      <img
+    <div className="flex justify-center">
+      <Image
         src={require("../../assets/bulletin_board1.png")}
-        width={200}
-        height={200}
+        width={500}
         alt="Bulletin board"
       />
     </div>
--- inkwell/app/page.tsx
+++ inkwell/app/page.tsx
@@ -3,13 +3,11 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen justify-center items-center">
       <Header />
-      <div className="text-center">
-        <button className="border rounded-full p-7 my-10 hover:bg-green-600 hover:text-white transition duration-300 ease-in">
-          <Link href="/log-in">Log in</Link>
-        </button>
-      </div>
-    </>
+      <button className="border rounded-full p-7 my-10 hover:bg-green-600 hover:text-white transition duration-300 ease-in">
+        <Link href="/log-in">Log in</Link>
+      </button>
+    </div>
   );
 }
