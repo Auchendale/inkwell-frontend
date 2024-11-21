0cdb438b6fd8495dc5ffb9708a7d70d1c9238a57
bs-jenk <jenkinson.sam@gmail.com>
2024-11-21 22:01:17 +0000

    adjust navigation links

--- inkwell/app/components/Nav.tsx
+++ inkwell/app/components/Nav.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="flex gap-4 p-4 bg-gray-800 shadow">
+    <nav className="flex gap-8 p-4 bg-gray-800 shadow">
       <Link href="/" className="hover:text-blue-500">
         Home
       </Link>
       <Link href="/log-in" className="hover:text-blue-500">
-        Change user
+        Change User
       </Link>
       <Link href="/bulletin-page" className="hover:text-blue-500">
-        Bulletin board
+        Bulletin Board
       </Link>
       <Link href="/" className="hover:text-blue-500">
-        Log out
+        Log Out
       </Link>
     </nav>
   );
