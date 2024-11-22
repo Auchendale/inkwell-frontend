c00063e5b8de13888c2e6776d93168ab6bf4ae9b
bs-jenk <jenkinson.sam@gmail.com>
2024-11-22 10:17:34 +0000

    create component for rendering pigeon holes

---
+++ inkwell/app/components/PigeonHoles.tsx
@@ -0,0 +1,9 @@
+const PigeonHoles = () => {
+  return (
+    <div className="border-4 border-black m-20 p-60">
+      <p className="text-center">display pigeon holes here</p>
+    </div>
+  );
+};
+
+export default PigeonHoles;
--- inkwell/app/user/page.tsx
+++ inkwell/app/user/page.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import PigeonHoles from "../components/PigeonHoles";
 import UserDetails from "../components/UserDetails";
 
 export default function UserPage() {
@@ -7,6 +8,7 @@ export default function UserPage() {
     <section>
       <Nav />
       <Header />
+      <PigeonHoles />
       <UserDetails />
     </section>
   );
