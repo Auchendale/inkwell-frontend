6e1465b19ba3c43d0faf60e47be1c65ef6788449
bs-jenk <jenkinson.sam@gmail.com>
2024-11-22 11:17:34 +0000

    adjust log in button styling

--- inkwell/app/components/LogInBtn.tsx
+++ inkwell/app/components/LogInBtn.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const LogInBtn = () => {
   return (
-    <button className="border rounded-full p-7 my-10 hover:bg-green-600 hover:text-white transition duration-300 ease-in">
+    <button className="btn">
       <Link href="/log-in">Log in</Link>
     </button>
   );
