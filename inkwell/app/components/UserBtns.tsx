5db657e0dba67ae7ee5e80dedd513ed363bb7349
bs-jenk <jenkinson.sam@gmail.com>
2024-11-22 11:06:41 +0000

    create user btns component and update user page layout

--- inkwell/app/components/UserBtns.tsx
+++ inkwell/app/components/UserBtns.tsx
@@ -1,21 +1,23 @@
+import UserDetails from "./UserDetails";
+
 const UserBtns = () => {
   return (
-    <div className="p-5 my-5 gap-10 grid grid-cols-3">
-      <button className="border rounded-full py-7 my-10 hover:bg-blue-600 hover:text-white transition duration-300 ease-in">
-        Write a new letter
-      </button>
-      <button className="border rounded-full py-7 my-10 hover:bg-blue-600 hover:text-white transition duration-300 ease-in">
-        Post on the bulletin board
-      </button>
-      <button className="border rounded-full py-7 my-10 hover:bg-blue-600 hover:text-white transition duration-300 ease-in">
-        User info
-      </button>
-      <button className="border rounded-full py-7 my-10 hover:bg-blue-600 hover:text-white transition duration-300 ease-in">
-        Saved letters
-      </button>
-      <button className="border rounded-full py-7 my-10 hover:bg-blue-600 hover:text-white transition duration-300 ease-in">
-        Settings
-      </button>
+    <div className="p-5 my-5 gap-10 grid grid-cols-2">
+      <button className="btn">Write a new letter</button>
+      <button className="btn">Post on the bulletin board</button>
+      <button className="btn">Saved letters</button>
+      <button className="btn">Settings</button>
+      <div className="dropdown">
+        <div tabIndex={0} role="button" className="btn">
+          User info
+        </div>
+        <div
+          tabIndex={0}
+          className="dropdown-content card bg-primary text-primary-content z-[1] w-64 p-2 shadow"
+        >
+          <UserDetails />
+        </div>
+      </div>
     </div>
   );
 };
