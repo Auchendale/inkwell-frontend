cdf2696d015035e96484a22659fc4efb04faecad
Charliesolo <charliesolomon99@googlemail.com>
2024-11-22 16:51:48 +0000

    add a drawing board and post it's contents to database

--- inkwell/app/components/DrawingPad.tsx
+++ inkwell/app/components/DrawingPad.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { Canvas, Rect } from "fabric";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState , useContext} from "react";
+import { UserContext } from "@/contexts/user-context";
+import axios from "axios";
 
 const DrawingPad = () => {
   const canvasRef = useRef(null);
   const [canvas, setCanvas] = useState(null);
+  const {user} = useContext(UserContext)
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -12,7 +15,7 @@ const DrawingPad = () => {
         width: 500,
         height: 500,
       });
-      initCanvas.backgroundColor = "#fff";
+      initCanvas.backgroundColor = "#fefcaf";
       initCanvas.renderAll();
 
       setCanvas(initCanvas);
@@ -32,16 +35,36 @@ const DrawingPad = () => {
         height: 75,
         fill: "#a084042"
       })
-
       canvas.add(rect)
     }
-
   }
 
+    const sendLetter = () => {
+
+      if(canvas){
+        const dataURL = canvas.toDataURL({
+          format: "png"
+        })
+        console.log(dataURL)
+        axios.post("https://inkwell-backend-kvij.onrender.com/api/letters", {sender: user.username ,recipient: "kieran",
+          content: {letter: dataURL}})
+          .then((response) => {
+            console.log(response)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      }
+
+
+    }
+
+
   return (
-    <div className="canvas-wrapper">
-      <button onClick={addRectangle}>add rectangle</button>
-      <canvas id="canvas" ref={canvasRef}></canvas>
+    <div className="h-screen flex flex-col items-center justify-center">
+      <button className="border-8 m-5" onClick={addRectangle}>add rectangle</button>
+      <canvas className="border-8 m-5" id="canvas" ref={canvasRef}></canvas>
+      <button className="border-8 m-10" onClick={sendLetter}>Send Letter</button>
     </div>
   );
 };
