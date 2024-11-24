87d0d81c2b509cf57227b361da11627ebd03b5bf
bs-jenk <jenkinson.sam@gmail.com>
2024-11-24 12:29:54 +0000

    add background image on home page

--- inkwell/app/page.tsx
+++ inkwell/app/page.tsx
@@ -1,11 +1,19 @@
 import Header from "./components/Header";
 import LogInBtn from "./components/LogInBtn";
+import bgImage from "../assets/bg-forest.jpeg";
 
 export default function Home() {
   return (
-    <section className="flex flex-col min-h-screen justify-center items-center">
-      <Header />
-      <LogInBtn />
-    </section>
+    <div
+      style={{
+        backgroundImage: `url(${bgImage.src})`,
+        backgroundSize: "cover",
+      }}
+    >
+      <section className="flex flex-col min-h-screen justify-center items-center">
+        <Header />
+        <LogInBtn />
+      </section>
+    </div>
   );
 }
