a97783d51235eb626ba6f72f4df2a9e0097da1e4
Oscar <oscarchow1230@gmail.com>
2024-11-25 12:52:06 +0000

    add home background stretch

--- inkwell/app/components/HomeBackground.tsx
+++ inkwell/app/components/HomeBackground.tsx
@@ -12,6 +12,7 @@ const HomeBackground = () => {
         source:
           "https://images.unsplash.com/photo-1487621167305-5d248087c724?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         blendingMode: "multiply",
+        stretchMode: ["stretch", "stretch"],
       },
       states: {
         "default-state": {
