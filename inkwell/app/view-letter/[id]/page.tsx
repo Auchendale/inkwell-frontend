4d8e35b79eaaa2ed1ff91e7bd24d63cf36f9db44
UArukonda <uarukonda@gmail.com>
2024-11-25 12:27:59 +0000

    display the selected letter

---
+++ inkwell/app/view-letter/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import SelectedLetter from "@/app/components/SelectedLetter";
+import { useParams } from "next/navigation";
+
+export default function ViewLetter() {
+  const params = useParams<{ id: string }>();
+
+  return (
+    <>
+      <SelectedLetter id={params.id} />
+    </>
+  );
+}
