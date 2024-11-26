4eabac48d4769e614eb763884fe3268d0a9b237d
Oscar <oscarchow1230@gmail.com>
2024-11-26 16:11:31 +0000

    add loading state to letters

--- inkwell/app/components/LetterViewer.tsx
+++ inkwell/app/components/LetterViewer.tsx
@@ -4,13 +4,14 @@ import axios from "axios";
 import { UserContext } from "@/contexts/user-context";
 import Link from "next/link";
 import Image from "next/image";
-const emptyBox0 = require("../../assets/emptyBox0.png")
-const emptyBox1 = require("../../assets/emptyBox1.png")
-const emptyBox2 = require("../../assets/emptyBox2.png")
-const emptyBox3 = require("../../assets/emptyBox3.png")
-const letterInBox1 = require("../../assets/letterInBox1.png")
-const letterInBox2 = require("../../assets/letterInBox2.png")
-const letterInBox3 = require("../../assets/letterInBox3.png")
+import LoadingBar from "./LoadingBar";
+const emptyBox0 = require("../../assets/emptyBox0.png");
+const emptyBox1 = require("../../assets/emptyBox1.png");
+const emptyBox2 = require("../../assets/emptyBox2.png");
+const emptyBox3 = require("../../assets/emptyBox3.png");
+const letterInBox1 = require("../../assets/letterInBox1.png");
+const letterInBox2 = require("../../assets/letterInBox2.png");
+const letterInBox3 = require("../../assets/letterInBox3.png");
 
 type LetterType = {
   _id: string;
@@ -33,58 +34,75 @@ const emptyLetter = {
   },
   is_opened: true,
   is_saved: false,
-  date_sent: "skoo"
-}
+  date_sent: "skoo",
+};
 
 const LetterViewer = () => {
   // const [isClicked, setIsClicked] = useState(false);
   const [letters, setLetters] = useState<Array<LetterType>>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { user } = useContext(UserContext);
 
-  const shuffle = (array: LetterType[]) => { 
-    const shuffled = array.slice(); 
-    let currentIndex = shuffled.length; 
-    let temporaryValue, randomIndex; 
-    while (currentIndex !== 0) { 
-      randomIndex = Math.floor(Math.random() * currentIndex); 
-      currentIndex -= 1; 
-      temporaryValue = shuffled[currentIndex]; 
-      shuffled[currentIndex] = shuffled[randomIndex]; 
-      shuffled[randomIndex] = temporaryValue; 
+  const shuffle = (array: LetterType[]) => {
+    const shuffled = array.slice();
+    let currentIndex = shuffled.length;
+    let temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = shuffled[currentIndex];
+      shuffled[currentIndex] = shuffled[randomIndex];
+      shuffled[randomIndex] = temporaryValue;
     }
-    return shuffled; 
+    return shuffled;
   };
 
   const randomEmptyBox = () => {
-    const arr = [emptyBox0, emptyBox0, emptyBox0, emptyBox1, emptyBox1, emptyBox1, emptyBox2, emptyBox2, emptyBox2, emptyBox3]
-    const item = arr[Math.floor(Math.random()*arr.length)]
-    return item
-  }
+    const arr = [
+      emptyBox0,
+      emptyBox0,
+      emptyBox0,
+      emptyBox1,
+      emptyBox1,
+      emptyBox1,
+      emptyBox2,
+      emptyBox2,
+      emptyBox2,
+      emptyBox3,
+    ];
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    return item;
+  };
 
   const randomLetter = () => {
-    const arr = [letterInBox1, letterInBox2, letterInBox3]
-    const item = arr[Math.floor(Math.random()*arr.length)]
-    return item
-
-  }
+    const arr = [letterInBox1, letterInBox2, letterInBox3];
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    return item;
+  };
   useEffect(() => {
     axios
       .get("https://inkwell-backend-j9si.onrender.com/api/letters", {
         params: {
           recipient: user.username,
-          is_opened: false
+          is_opened: false,
+          limit: 21,
         },
       })
       .then((res) => {
-        const letterArray: Array<LetterType> = [...res.data.letters]
-        while(letterArray.length < 21){
-          letterArray.push(emptyLetter)
+        const letterArray: Array<LetterType> = [...res.data.letters];
+        while (letterArray.length < 21) {
+          letterArray.push(emptyLetter);
         }
-        const shuffledArr = shuffle(letterArray)
+        const shuffledArr = shuffle(letterArray);
         setLetters(shuffledArr);
+        setIsLoading(false);
       })
       .catch((err) => err);
   }, []);
+
+  if (isLoading) {
+    return <LoadingBar />;
+  }
 
   // const toggleImage = (event: unknown) => {
   //   setIsClicked(true);
@@ -100,26 +118,38 @@ const LetterViewer = () => {
 
   return (
     <>
-    <ul className="grid grid-cols-7 grid-rows-3 gap-5 bg-yellow-900">
-      {letters.map((letter: LetterType, index: number) => {
-        return (
-         <>
-         {letter.is_opened === false ? 
-         <li key={letter._id} className="border border-amber-900	">
-          <Link href={`/view-letter/${letter._id}`}>
-            <Image src={randomLetter()} alt="Hand-drawn pigeon hole with letter inside" width={500}/>
-          </Link>
-         </li> : 
-          
-         <li key={`emptybox${index}`} className="border border-amber-900">
-            <Image src={randomEmptyBox()} alt="Hand-drawn empty pigeon hole" width={500}/>
-          </li>}
-         </>
-        )
-      })}
-    </ul>
+      <ul className="grid grid-cols-7 grid-rows-3 gap-5 bg-yellow-900">
+        {letters.map((letter: LetterType, index: number) => {
+          return (
+            <>
+              {letter.is_opened === false ? (
+                <li key={letter._id} className="border border-amber-900	">
+                  <Link href={`/view-letter/${letter._id}`}>
+                    <Image
+                      src={randomLetter()}
+                      alt="Hand-drawn pigeon hole with letter inside"
+                      width={500}
+                    />
+                  </Link>
+                </li>
+              ) : (
+                <li
+                  key={`emptybox${index}`}
+                  className="border border-amber-900"
+                >
+                  <Image
+                    src={randomEmptyBox()}
+                    alt="Hand-drawn empty pigeon hole"
+                    width={500}
+                  />
+                </li>
+              )}
+            </>
+          );
+        })}
+      </ul>
     </>
-    )
+  );
 };
 
 export default LetterViewer;
