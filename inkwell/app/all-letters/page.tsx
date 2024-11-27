de0dee32beaffe6ec84791984a154745b629ada0
Charliesolo <charliesolomon99@googlemail.com>
2024-11-27 15:56:12 +0000

    add view all letters page and allow sorting

---
+++ inkwell/app/all-letters/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import DarkLogo from "../components/DarkLogo";
+import Nav from "../components/Nav";
+import AllLetters from "../components/AllLetters";
+
+function page() {
+  return (
+    <>
+      <Nav />
+      <DarkLogo />
+      <AllLetters />
+    </>
+  );
+}
+
+export default page;
