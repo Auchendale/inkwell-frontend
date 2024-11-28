db7e66a6e3028b710cd30a98df0c7cfdd9701831
bs-jenk <jenkinson.sam@gmail.com>
2024-11-28 12:26:57 +0000

    adjust rendered components and welcome message on user and bulletin pages

--- inkwell/app/bulletin-page/page.tsx
+++ inkwell/app/bulletin-page/page.tsx
@@ -1,14 +1,12 @@
 import BulletinBoard from "../components/BulletinBoard";
 import DarkLogo from "../components/DarkLogo";
 import Nav from "../components/Nav";
-import WelcomeMsg from "../components/WelcomeMsg";
 
 export default function BulletinPage() {
   return (
     <div>
       <Nav />
       <DarkLogo />
-      <WelcomeMsg />
       <BulletinBoard />
     </div>
   );
